9cfe0e237d87371bb61ad002a22cd503c861740f
Berko Szabolcs <szberko@gmail.com>
2016-06-12 18:41:04 +0200

    add ability to draw a marker with info window. add new dialog to choose which entity the user wants to add to the map

--- Reimagined-tribble/app/src/main/java/com/ftn/android/reimagined_tribble/activities/MapsActivity.java
+++ Reimagined-tribble/app/src/main/java/com/ftn/android/reimagined_tribble/activities/MapsActivity.java
@@ -3,6 +3,7 @@ package com.ftn.android.reimagined_tribble.activities;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
@@ -11,7 +12,12 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
+import com.afollestad.materialdialogs.MaterialDialog;
+import com.afollestad.materialdialogs.simplelist.MaterialSimpleListAdapter;
+import com.afollestad.materialdialogs.simplelist.MaterialSimpleListItem;
 import com.ftn.android.reimagined_tribble.R;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -20,10 +26,12 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener, GoogleMap.OnInfoWindowClickListener {
 
-    private GoogleMap googleMap;
+    GoogleMap googleMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -89,8 +97,36 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         CameraUpdate camUpdate = CameraUpdateFactory.newCameraPosition(camPos);
         googleMap.moveCamera(camUpdate);
+
+        googleMap.setOnMapClickListener(this);
+
     }
 
+    @Override
+    public void onMapClick(LatLng latLng) {
+        // Creating a marker
+        MarkerOptions markerOptions = new MarkerOptions();
+
+        // Setting the position for the marker
+        markerOptions.position(latLng);
+
+        // Clears the previously touched position
+        googleMap.clear();
+
+        // Animating to the touched position
+        googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+
+        // Placing a marker on the touched position
+//        googleMap.addMarker(markerOptions);
+
+        googleMap.setInfoWindowAdapter(new AddNewInfoWindowAdapter());
+        googleMap.setOnInfoWindowClickListener(this);
+
+        Marker marker = googleMap.addMarker(markerOptions);
+        marker.showInfoWindow();
+
+
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -107,4 +143,63 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         Intent settingsIntent = new Intent(getApplicationContext(), SettingsActivity.class);
         startActivity(settingsIntent);
     }
+
+    class AddNewInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
+
+        private final View mWindow;
+
+        public AddNewInfoWindowAdapter() {
+            mWindow = getLayoutInflater().inflate(R.layout.map_info_window, null);
+        }
+
+        @Override
+        public View getInfoWindow(Marker marker) {
+            return mWindow;
+        }
+
+        @Override
+        public View getInfoContents(Marker marker) {
+            return null;
+        }
+
+    }
+
+    @Override
+    public void onInfoWindowClick(Marker marker) {
+        googleMap.clear();
+
+        final MaterialSimpleListAdapter adapter = new MaterialSimpleListAdapter(this);
+        adapter.add(new MaterialSimpleListItem.Builder(this)
+                .content(R.string.add_new_gas_station_from_map)
+                .icon(R.drawable.ic_gas_station_add_new_from_map)
+                .backgroundColor(Color.WHITE)
+                .build());
+        adapter.add(new MaterialSimpleListItem.Builder(this)
+                .content(R.string.add_new_incident_from_map)
+                .icon(R.drawable.ic_incident_add_new_from_map)
+                .backgroundColor(Color.WHITE)
+                .build());
+
+        new MaterialDialog.Builder(this)
+                .title(R.string.add_new_dialog_title)
+                .adapter(adapter, new MaterialDialog.ListCallback() {
+                    @Override
+                    public void onSelection(MaterialDialog dialog, View itemView, int which, CharSequence text) {
+                        MaterialSimpleListItem item = adapter.getItem(which);
+                        showToast(item.getContent().toString());
+                    }
+                })
+                .show();
+    }
+
+    private Toast mToast;
+    private void showToast(String message) {
+        if (mToast != null) {
+            mToast.cancel();
+            mToast = null;
+        }
+        mToast = Toast.makeText(this, message, Toast.LENGTH_SHORT);
+        mToast.show();
+    }
+
 }
