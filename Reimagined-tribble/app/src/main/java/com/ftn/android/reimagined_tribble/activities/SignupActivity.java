4ce0a016c2726e05efb5587f227250c9fb1883fd
Berko Szabolcs <szberko@gmail.com>
2016-06-18 00:52:20 +0200

    cleanp styles.xml. create a complete theme for the application

--- Reimagined-tribble/app/src/main/java/com/ftn/android/reimagined_tribble/activities/SignupActivity.java
+++ Reimagined-tribble/app/src/main/java/com/ftn/android/reimagined_tribble/activities/SignupActivity.java
@@ -57,7 +57,7 @@ public class SignupActivity extends AppCompatActivity {
         _signupButton.setEnabled(false);
 
         final ProgressDialog progressDialog = new ProgressDialog(SignupActivity.this,
-                R.style.AppTheme_Dark_Dialog);
+                R.style.SignUpDialog);
         progressDialog.setIndeterminate(true);
         progressDialog.setMessage("Creating Account...");
         progressDialog.show();
