2f58b6425232effa8d2386407036be0fed16ff88
Berko Szabolcs <szberko@gmail.com>
2016-06-23 00:41:13 +0200

    implement androidannotations in already existing activities

---
+++ Reimagined-tribble/app/src/main/java/com/ftn/android/reimagined_tribble/adapters/AddInfoWindowAdapter.java
@@ -0,0 +1,31 @@
+package com.ftn.android.reimagined_tribble.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+
+import com.ftn.android.reimagined_tribble.R;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.Marker;
+
+/**
+ * Created by szberko
+ */
+public class AddInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
+
+    private final View mWindow;
+
+    public AddInfoWindowAdapter(LayoutInflater layoutInflater) {
+        mWindow = layoutInflater.inflate(R.layout.map_info_window, null);
+    }
+
+    @Override
+    public View getInfoWindow(Marker marker) {
+        return mWindow;
+    }
+
+    @Override
+    public View getInfoContents(Marker marker) {
+        return null;
+    }
+
+}
