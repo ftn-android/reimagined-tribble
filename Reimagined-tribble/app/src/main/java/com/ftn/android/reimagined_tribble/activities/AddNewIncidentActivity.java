e60612c8f18200c0d707397f34c887586df702d4
Berko Szabolcs <szberko@gmail.com>
2016-06-29 00:30:10 +0200

    finalizing the design of add new incident activity

--- Reimagined-tribble/app/src/main/java/com/ftn/android/reimagined_tribble/activities/AddNewIncidentActivity.java
+++ Reimagined-tribble/app/src/main/java/com/ftn/android/reimagined_tribble/activities/AddNewIncidentActivity.java
@@ -1,30 +1,99 @@
 package com.ftn.android.reimagined_tribble.activities;
 
+import android.content.Intent;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.ftn.android.reimagined_tribble.R;
 
 import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.OptionsItem;
+import org.androidannotations.annotations.OptionsMenu;
+import org.androidannotations.annotations.ViewById;
+import org.androidannotations.annotations.res.StringRes;
+
+import java.io.File;
+
+import pl.aprilapps.easyphotopicker.DefaultCallback;
+import pl.aprilapps.easyphotopicker.EasyImage;
 
 /**
  * Created by ftn/tim
  */
 
 @EActivity(R.layout.add_new_incident_activity)
+@OptionsMenu(R.menu.add_new_menu)
 public class AddNewIncidentActivity extends AppCompatActivity{
+
+    @ViewById(R.id.incident_type)
+    Spinner spinner;
+
+    @StringRes(R.string.title_add_new_incident)
+    String activityTitle;
 
     @AfterViews
     protected void init(){
-        Spinner spinner = (Spinner) findViewById(R.id.incident_type);
-// Create an ArrayAdapter using the string array and a default spinner layout
+        //Toolbar and back button setup
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        CollapsingToolbarLayout collapsingToolbar =
+                (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        collapsingToolbar.setTitle(activityTitle);
+
+        //Spinner setup
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.incident_types, android.R.layout.simple_spinner_item);
-// Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-// Apply the adapter to the spinner
         spinner.setAdapter(adapter);
+
+        final ImageView imageView = (ImageView) findViewById(R.id.backdrop);
+        Glide.with(this).load("").error(R.drawable.ic_photo_placeholder).into(imageView);
+    }
+
+    @Click(R.id.fab_add_new_incident_details)
+    protected void startTheCamera(){
+        EasyImage.openCamera(AddNewIncidentActivity.this, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        EasyImage.handleActivityResult(requestCode, resultCode, data, this, new DefaultCallback() {
+            @Override
+            public void onImagePickerError(Exception e, EasyImage.ImageSource source, int type) {
+                //Some error handling
+            }
+
+            @Override
+            public void onImagePicked(File imageFile, EasyImage.ImageSource source, int type) {
+                //Handle the image
+                ImageView headerPhoto = (ImageView) findViewById(R.id.backdrop);
+                Glide.with(AddNewIncidentActivity.this).load(imageFile).centerCrop().into(headerPhoto);
+            }
+
+            @Override
+            public void onCanceled(EasyImage.ImageSource source, int type) {
+                //Cancel handling, you might wanna remove taken photo if it was canceled
+                if (source == EasyImage.ImageSource.CAMERA) {
+                    File photoFile = EasyImage.lastlyTakenButCanceledPhoto(AddNewIncidentActivity.this);
+                    if (photoFile != null) photoFile.delete();
+                }
+            }
+        });
+    }
+
+    @OptionsItem(R.id.add)
+    protected void clickOnAddIncident(){
+        Toast.makeText(this, "Add button", Toast.LENGTH_LONG).show();
     }
 }
