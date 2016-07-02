6a53f4dc8997743fab323aff7b9676e5b1047e4f
Pazzo92 <frank.filip@hotmail.com>
2016-07-02 16:48:54 +0200

    add custom Info window adapter for the already existing Incidents and Gas Stations.

--- Reimagined-tribble/app/src/main/java/com/ftn/android/reimagined_tribble/adapters/ViewInfoWindowAdapter.java
+++ Reimagined-tribble/app/src/main/java/com/ftn/android/reimagined_tribble/adapters/ViewInfoWindowAdapter.java
@@ -1,9 +1,14 @@
 package com.ftn.android.reimagined_tribble.adapters;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.ftn.android.reimagined_tribble.R;
+import com.ftn.android.reimagined_tribble.model.Entity;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.Marker;
 
@@ -13,19 +18,31 @@ import com.google.android.gms.maps.model.Marker;
 public class ViewInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
 
     private final View mWindow;
+    private Entity entity;
 
-    public ViewInfoWindowAdapter(LayoutInflater layoutInflater) {
+    public ViewInfoWindowAdapter(LayoutInflater layoutInflater, Entity entity) {
         mWindow = layoutInflater.inflate(R.layout.map_view_info_window, null);
+        this.entity = entity;
     }
 
     @Override
     public View getInfoWindow(Marker marker) {
-        return mWindow;
+        return null;
     }
 
     @Override
     public View getInfoContents(Marker marker) {
-        return null;
+        ImageView badgeImageView = (ImageView) mWindow.findViewById(R.id.view_info_window_badge);
+        Bitmap bitmap = BitmapFactory.decodeByteArray(entity.getImage(), 0, entity.getImage().length);
+        badgeImageView.setImageBitmap(bitmap);
+
+        TextView titleTextView = (TextView) mWindow.findViewById(R.id.view_info_window_title);
+        titleTextView.setText(entity.getName());
+
+        TextView snippetTextView = (TextView) mWindow.findViewById(R.id.view_info_window_snippet);
+        snippetTextView.setText(entity.getDescription());
+
+        return mWindow;
     }
 
 }
