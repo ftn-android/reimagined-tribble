a2cd62685652ec9cda0d7afaf465834ea82b9db0
Pazzo92 <frank.filip@hotmail.com>
2016-07-02 16:47:49 +0200

    add interface with the common getter methods for the incident and gas station for later usage

---
+++ Reimagined-tribble/app/src/main/java/com/ftn/android/reimagined_tribble/model/Entity.java
@@ -0,0 +1,11 @@
+package com.ftn.android.reimagined_tribble.model;
+
+/**
+ * Created by szberko
+ */
+public interface Entity {
+
+    public String getName();
+    public String getDescription();
+    public byte[] getImage();
+}
