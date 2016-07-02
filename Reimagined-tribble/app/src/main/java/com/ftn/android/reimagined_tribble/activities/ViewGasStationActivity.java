69030a8c001242d8c9607c08b74abcf517d33e2c
Pazzo92 <frank.filip@hotmail.com>
2016-07-02 20:19:40 +0200

    load passed data into the field of the ViewGasStationActivity

--- Reimagined-tribble/app/src/main/java/com/ftn/android/reimagined_tribble/activities/ViewGasStationActivity.java
+++ Reimagined-tribble/app/src/main/java/com/ftn/android/reimagined_tribble/activities/ViewGasStationActivity.java
@@ -1,19 +1,29 @@
 package com.ftn.android.reimagined_tribble.activities;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.ftn.android.reimagined_tribble.R;
+import com.ftn.android.reimagined_tribble.model.GasStation;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.Extra;
 import org.androidannotations.annotations.ViewById;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 /**
  * Created by ftn/tim
@@ -24,6 +34,23 @@ public class ViewGasStationActivity extends AppCompatActivity {
     @ViewById(R.id.view_gas_station_container)
     LinearLayout container;
 
+    @Extra
+    GasStation chosenGasStation;
+
+    @ViewById(R.id.view_gas_station_name)
+    TextView gasStationName;
+
+    @ViewById(R.id.view_gas_station_street)
+    TextView gasStationStreetName;
+
+    @ViewById(R.id.view_gas_station_city)
+    TextView gasStationCityName;
+
+    @ViewById(R.id.view_gas_station_created_on)
+    TextView gasStationCreatedOn;
+
+    @ViewById(R.id.view_gas_station_description)
+    TextView gasStationDescription;
 
     @AfterViews
     protected void init(){
@@ -33,10 +60,29 @@ public class ViewGasStationActivity extends AppCompatActivity {
 
         CollapsingToolbarLayout collapsingToolbar =
                 (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
-        collapsingToolbar.setTitle("placeholder");
+        collapsingToolbar.setTitle(chosenGasStation.getName());
 
         final ImageView imageView = (ImageView) findViewById(R.id.backdrop);
-        Glide.with(this).load("").error(R.drawable.ic_photo_placeholder).into(imageView);
+        Glide.with(this).load(chosenGasStation.getImage()).error(R.drawable.ic_photo_placeholder).into(imageView);
+
+        Geocoder geocoder;
+        List<Address> addresses = new ArrayList<>();
+        geocoder = new Geocoder(this, Locale.getDefault());
+
+        try {
+            addresses = geocoder.getFromLocation(chosenGasStation.getLattitude(), chosenGasStation.getLongittude(), 1); // Here 1 represent max location result to returned, by documents it recommended 1 to 5
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String address = addresses.get(0).getAddressLine(0);
+        String cityCountry = addresses.get(0).getLocality() + ", " + addresses.get(0).getCountryName();
+
+        gasStationName.setText(chosenGasStation.getName());
+        gasStationCityName.setText(cityCountry);
+        gasStationStreetName.setText(address);
+        gasStationCreatedOn.setText(chosenGasStation.getDate());
+        gasStationDescription.setText(chosenGasStation.getDescription());
     }
 
     @Click(R.id.fab_view_gas_station)
